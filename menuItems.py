4e807c6328ab7e8201cb9b87403badd28cfc8b98
Advik Arora <144552518+advikarora@users.noreply.github.com>
2024-01-22 11:43:28 -0500

    Update menuItems.py

--- menuItems.py
+++ menuItems.py
@@ -1,30 +1,24 @@
+#returns menu items for a day
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
 
-def scrape_menu(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            menu_items = []
-            menu_items1 = soup.find_all('a', class_=lambda x: x and 'gluten' in x)
-            menu_items2 = soup.find_all('a', class_=lambda x: x and 'show-nutrition' in x) 
-            if menu_items1 and menu_items2:
-                for menu_item in menu_items1:
-                    menu_items.append(menu_item.get_text(strip=True))
-                for menu_item in menu_items2:
-                    menu_items.append(menu_item.get_text(strip=True))
-                print(menu_items)
-            else:
-                print("No menu items found with the specified setup.")
-        else:
-            print(f"Failed to retrieve the page. Status code: {response.status_code}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-current_date = datetime.now().strftime("%Y-%m-%d")
-original_url = "https://dining.unc.edu/locations/chase/?date=2024-1-8"
-new_url = original_url.replace("2023-11-16", current_date)
-
-scrape_menu(new_url)
+def get_menu_items(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser') 
+    mealTimes = soup.find_all('div', class_= ['c-tab', 'is-active'])
+    array1 = []
+    array2 = []
+    final = []
+    menuItemsPerStation = []
+    menuItemsPerMealTime = []
+    for t in range(len(mealTimes)):
+        elements1 = mealTimes[t].find_all('div', id=lambda x: x and x.startswith('menu-station-data'))
+        array1 += elements1
+        menuItemsPerMealTime.append(len(elements1))
+    for t in range(len(array1)):
+        elements2 = array1[t].find_all('li', class_=lambda x: x and 'menu-item' in x)
+        array2 += elements2
+        menuItemsPerStation.append(len(elements2))
+    for item in array2:
+        final.append(item.get_text(strip = True))
+    return final, menuItemsPerStation
